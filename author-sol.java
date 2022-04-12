b787ffa5b8b34501fb19ff08e59ce36b2fba90ec
Mark Jason Galang <51401355+mgalang229@users.noreply.github.com>
2022-04-12 14:22:58 +0800

    Create author-sol.java

---
+++ author-sol.java
@@ -0,0 +1,92 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) {	
+		FastScanner fs = new FastScanner();
+		PrintWriter out = new PrintWriter(System.out);
+		int T = 1;
+		T = fs.nextInt();
+		for(int tc = 0; tc < T; tc++) {
+			long a = fs.nextLong(), b = fs.nextLong();
+			if(Math.min(a, b) <= 1) {
+				out.println(-1);
+				continue;
+			}
+			out.println(getGCD(a, b) > 1 ? 0 : 1);
+		}
+		out.close();
+	}
+	
+	static long getGCD(long a, long b) {
+		if(b == 0) {
+			return a;
+		}
+		return getGCD(b, a % b);
+	}
+	
+	/*
+	static void sort(int[] a) {
+		ArrayList<Integer> arr = new ArrayList<>();
+		for(int x : a) {
+			arr.add(x);
+		}
+		Collections.sort(arr);
+		for(int i = 0; i < a.length; i++) {
+			a[i] = arr.get(i);
+		}
+	}
+	*/
+		
+	static class FastScanner {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer("");
+		
+		String next() {
+			while(!st.hasMoreTokens()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch(IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		
+		int[] readArray(int n) {
+			int[] a = new int[n];
+			for(int i = 0; i < n; i++) {
+				a[i] = nextInt();
+			}
+			return a;
+		}
+		
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+}
